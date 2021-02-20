ac5f46fba248a5a1ea1a988d74691a39f0b1839b
Jesse Hoogland <jessequinten@gmail.com>
2021-02-20 11:44:09 +0100

    CHG Updating a few arguments to be boolean flags

--- process.py
+++ process.py
@@ -10,35 +10,47 @@ from speech.speaker import *
 
 parser = argparse.ArgumentParser(description=__doc__)
 
-parser.add_argument('-p', '--file_path', type=str, default="",
+parser.add_argument('-p', '--file-path', type=str, default="",
                     help='The location of the .wav audiofile to play.')
 
-parser.add_argument('-P', '--pitch_range', type=int, nargs=2, default=[75, 500],
+parser.add_argument('-P', '--pitch-range', type=int, nargs=2, default=[75, 500],
                     help='')
 
-parser.add_argument('-O', '--octave_cost', type=float, default=0.01,
+parser.add_argument('-O', '--octave-cost', type=float, default=0.01,
                     help='')
 
-parser.add_argument('-V', '--voicing_threshold', type=float, default=0.45,
+parser.add_argument('-V', '--voicing-threshold', type=float, default=0.45,
                     help='')
 
-parser.add_argument('-S', '--silence_threshold', type=float, default=0.03,
+parser.add_argument('-S', '--silence-threshold', type=float, default=0.03,
                     help='')
 
-parser.add_argument('-J', '--octave_jump_cost', type=float, default=0.35,
+parser.add_argument('-J', '--octave-jump-cost', type=float, default=0.35,
                     help='')
 
-parser.add_argument('-T', '--voicing_transition_cost', type=float, default=0.14,
+parser.add_argument('-T', '--voicing-transition-cost', type=float, default=0.14,
                     help='')
 
-parser.add_argument('-c','--pitch_contour', type=str, default="boersma",
+parser.add_argument('-c','--pitch-contour', type=str, default="boersma",
                     help='The kind of pitch_contour to apply. Options are currently restricted to `boersma``')
 
-parser.add_argument('-r','--record', type=str, default="False",
-                    help='Whether to record input')
+parser.add_argument('-a', '--audio', dest="audio", action="store_true",
+                    help="Play the clip being analyzed. This is overriden by `-r`.")
+parser.add_argument('--no_audio', dest="audio", action="store_false", help="Do not play the audio clip behind analyzed. Default.")
+parser.set_defaults(audio=False)
 
-parser.add_argument('-v','--verbose', type=str, default="False",
+parser.add_argument('-r','--record', dest="record", action="store_true",
+                    help='Prompt the user to record their own voice. Overrides `-a`.')
+parser.add_argument('--no-record', dest="record", action="store_false",
+                    help='Prompt the user to not record their own voice. Default.')
+parser.set_defaults(record=False)
+
+parser.add_argument('-v','--verbose', dest="verbose", action="store_true",
                     help='Whether to record verbose output. Results in displaying more plots.')
+parser.add_argument('--no-verbose', dest="verbose", action="store_false",
+                    help='Hides additional plots. Default')
+parser.set_defaults(verbose=False)
+
 
 parser.add_argument('-s','--save', type=str, default="",
                     help='Path to save pitch contour to JSON. Default="" (does not save). ')
@@ -46,34 +58,39 @@ parser.add_argument('-s','--save', type=str, default="",
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    praat = F0Drawer(F0Analyzer())
-    # time.sleep(1)
+    f0 = F0Drawer(F0Analyzer())
 
-    settings = {"max_candidates_per_frame":15,
-                "min_pitch":args.pitch_range[0],
-                "max_pitch":args.pitch_range[1],
-                "voicing_threshold":args.voicing_threshold,
-                "silence_threshold":args.silence_threshold,
-                "octave_cost":args.octave_cost,
-                "voicing_transition_cost":args.voicing_transition_cost,
-                "octave_jump_cost":args.octave_jump_cost}
+    settings = {
+        "max_candidates_per_frame":15,
+        "min_pitch":args.pitch_range[0],
+        "max_pitch":args.pitch_range[1],
+        "voicing_threshold":args.voicing_threshold,
+        "silence_threshold":args.silence_threshold,
+        "octave_cost":args.octave_cost,
+        "voicing_transition_cost":args.voicing_transition_cost,
+        "octave_jump_cost":args.octave_jump_cost
+    }
 
-    # plt.plot(praat.signal[0:500])
-    # praat.listen()
-    # praat.draw_spectrograms(args.frame_duration, args.frame_stride, args.n_fft_points, args.n_filters)
+    # plt.plot(f0.signal[0:500])
+    # f0.listen()
+    # f0.draw_spectrograms(args.frame_duration, args.frame_stride, args.n_fft_points, args.n_filters)
 
     res = {}
     src_path, target_path = np.array([]), np.array([])
     if args.file_path != "":
         speech_file = SpeechFile(args.file_path)
 
-        if args.record == "True":
-            src_path, target_path = praat.record_and_compare(speech_file, **settings)
+        if args.record:
+            src_path, target_path = f0.record_and_compare(speech_file, **settings)
         else:
-            _, src_path = praat.draw_f0_contour(speech_file, **settings)
+            _, src_path = f0.draw_f0_contour(speech_file, **settings)
+
+            if args.audio:
+                speech_file.play()
+
 
     else:
-        _, target_path  = praat.record_and_draw(**settings)
+        _, target_path  = f0.record_and_draw(**settings)
 
     res["src_path"], res["target_path"] = src_path.tolist(), target_path.tolist()
 
